fd755b6d9947df822524ce88a944c4947d8ee770
anushka <singhashi305@gmail.com>
2024-11-16 12:34:44 +0530

    Add files via upload

--- Digital Clock.py
+++ Digital Clock.py
@@ -1,13 +1,30 @@
 import tkinter as tk
 from time import strftime
+from datetime import datetime
+
 
 root = tk.Tk()
 root.title ("Digital Clock")
 def time():
-    string=strftime('%H:%M:%S %p\n %D')
+    now = datetime.now()
+    string=strftime('%H:%M:%S %p\n')
+    current_date = now.strftime("%A, %d %B %Y")
+    specific_date = datetime(2024, 11, 16)  # Year, month, and day are mandatory
+
     label.config(text=string)
+    date_label.config(text=current_date)
     label.after(1000,time)
-label=tk.Label(root, font=('calibri',50,'bold'), background='light blue', foreground='black')
+    
+
+
+label=tk.Label(root, font=('calibri',47,'bold'), background='light blue', foreground='black')
+label.pack(anchor='w')
+date_label = tk.Label(root, font=("calibri", 20,'bold'), background="light blue", foreground="black")
 label.pack(anchor='center')
+date_label.pack()
+
 time()
+
+
+
 root.mainloop()
